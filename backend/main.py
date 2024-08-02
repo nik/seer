e86b4afb57b2d283032c8a9dc13c6a0c69ac34d5
nik kov <nickkov89@gmail.com>
2024-08-02 16:20:30 -0400

    delete unused

--- backend/main.py
+++ backend/main.py
@@ -18,11 +18,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     try:
